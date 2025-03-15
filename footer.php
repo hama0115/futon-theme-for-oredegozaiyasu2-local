f6a34f47cc873766958b2d0d33e6c2c90ed71f97
hama0115 <oh.akibee1231@gmail.com>
2025-03-15 16:32:04 +0900

    ファーストコミット

---
+++ footer.php
@@ -0,0 +1,15 @@
+    <footer class="footer">
+      <a href="<?php echo esc_url(home_url('/')); ?>" class="footer-logo"><?php bloginfo('name'); ?></a>
+      <p class="copyright"><?php echo $_SERVER['HTTP_HOST']; ?> all right reserved.</p>
+    </footer>
+
+    <?php wp_footer(); ?>
+
+    <!-- JavaScript -->
+
+      <!-- fontawesome(をいちおういれておく) -->
+      <script src="https://kit.fontawesome.com/9ab3ae9094.js" crossorigin="anonymous"></script>
+
+    <!--#JS-->
+  </body>
+</html>
