9cee321e130c44690dd83a06edd86a2ea6e67ccd
hama0115 <oh.akibee1231@gmail.com>
2025-03-20 11:00:22 +0900

    日付アーカイブリンクを作成,サイドバー作成

---
+++ sidebar.php
@@ -0,0 +1,8 @@
+<aside class="sidebar">
+  <div class="sidebar-block">
+    <h3>記事アーカイブ</h3>
+    <ul class="monthly-archive">
+      <?php wp_get_archives('type=monthly'); ?>
+    </ul>
+  </div>
+</aside>
