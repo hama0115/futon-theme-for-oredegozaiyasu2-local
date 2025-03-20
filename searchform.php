65a2d1f03608894a873b4d5960aaf29e7404033c
hama0115 <oh.akibee1231@gmail.com>
2025-03-20 16:00:26 +0900

    searchform.phpを作成

---
+++ searchform.php
@@ -0,0 +1,9 @@
+<form action="" class="form-inline kp-header-search-input" role="search">
+  <div class="input-group">
+    <label for="searchword">サイト内検索</label>
+    <input type="text" name="s" class="form-control" placeholder="検索を発動" id="searchword">
+    <div class="input-group-append">
+      <button class="btn btn-info" type="submit"><i class="fas fa-search"></i><span>検索</span></button>
+    </div>
+  </div>
+</form>
