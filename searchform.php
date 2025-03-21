db0478691f757d891528f52310a8cc84cf804b36
hama0115 <oh.akibee1231@gmail.com>
2025-03-21 16:39:46 +0900

    フッターの検索フォームをスタイリング

--- searchform.php
+++ searchform.php
@@ -1,8 +1,8 @@
 <!-- 「wordpressデザインレシピ集」を参考 -->
 <form action="" class="form-inline kp-header-search-input" role="search">
   <div class="input-group">
-    <label for="searchword">サイト内検索</label>
-    <input type="text" name="s" class="form-control" placeholder="検索を発動" id="searchword">
+    <label for="searchword">サイト内検索</label><!-- CSSで非表示 -->
+    <input type="text" name="s" class="form-control" id="searchword">
     <div class="input-group-append">
       <button class="btn btn-info" type="submit"><i class="fas fa-search"></i><span>検索</span></button>
     </div>
