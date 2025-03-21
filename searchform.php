d7c793d78e6061ccdfa920c2800a54b5922486e7
hama0115 <oh.akibee1231@gmail.com>
2025-03-21 17:04:50 +0900

    検索フォームのスタイルを修正

--- searchform.php
+++ searchform.php
@@ -1,10 +1,12 @@
 <!-- 「wordpressデザインレシピ集」を参考 -->
-<form action="" class="form-inline kp-header-search-input" role="search">
+<form id="searchform" method="GET" action="/" class="form-inline kp-header-searchinput" role="search">
   <div class="input-group">
     <label for="searchword">サイト内検索</label><!-- CSSで非表示 -->
     <input type="text" name="s" class="form-control" id="searchword">
     <div class="input-group-append">
-      <button class="btn btn-info" type="submit"><i class="fas fa-search"></i><span>検索</span></button>
+      <button class="btn btn-info" type="submit">
+        <i class="fas fa-search"></i><span>検索</span>
+      </button>
     </div>
   </div>
 </form>
